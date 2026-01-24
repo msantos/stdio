9e25b373b10b10957e0bdf97e5790b428b0bdcc1
Michael Santos <michael.santos@gmail.com>
2026-01-24 10:08:36 -0500

    doc: fix mermaid diagrams
    Mermaid diagrams aren't rendered on hex.pm. Use the mermaid example from:
    
    https://hexdocs.pm/ex_doc/readme.html#rendering-mermaid-graphs

--- mix.exs
+++ mix.exs
@@ -116,8 +116,34 @@ defmodule Stdio.MixProject do
       before_closing_body_tag: fn
         :html ->
           """
-          <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
-          <script>mermaid.initialize({startOnLoad: true})</script>
+          <script defer src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+          <script>
+          let initialized = false;
+
+          window.addEventListener("exdoc:loaded", () => {
+          if (!initialized) {
+          mermaid.initialize({
+          startOnLoad: false,
+          theme: document.body.className.includes("dark") ? "dark" : "default"
+          });
+          initialized = true;
+          }
+
+          let id = 0;
+          for (const codeEl of document.querySelectorAll("pre code.mermaid")) {
+          const preEl = codeEl.parentElement;
+          const graphDefinition = codeEl.textContent;
+          const graphEl = document.createElement("div");
+          const graphId = "mermaid-graph-" + id++;
+          mermaid.render(graphId, graphDefinition).then(({svg, bindFunctions}) => {
+          graphEl.innerHTML = svg;
+          bindFunctions?.(graphEl);
+          preEl.insertAdjacentElement("afterend", graphEl);
+          preEl.remove();
+          });
+          }
+          });
+          </script>
           """
 
         _ ->
